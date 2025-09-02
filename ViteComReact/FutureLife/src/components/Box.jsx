0a727240f1bd3082895c402ad3060e985ac7a652
ToxickGreen <luizgarciagarcia5@gmail.com>
2025-09-02 15:29:59 -0400

    Adicionando flexbox de entrada

---
+++ ViteComReact/FutureLife/src/components/Box.jsx
@@ -0,0 +1,14 @@
+function Box() {
+    let entrada = 1500;
+    return(
+        <>
+            <div>
+                <p>Entrada</p> <br />
+                {entrada}
+            </div>
+        </>
+    );
+
+}
+
+export default Box;
