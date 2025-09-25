c8ed4f7a95da6432738466b273b5f9c5a7f684e4
ToxickGreen <luizgarciagarcia5@gmail.com>
2025-09-25 18:41:21 -0400

    inicio da mecânica da lista

--- ViteComReact/FutureLife/src/components/Lista.jsx
+++ ViteComReact/FutureLife/src/components/Lista.jsx
@@ -1,17 +1,56 @@
+import { useState } from "react";
+
 function Lista (){
 
-    let list = {
-        desc: null,
-        valor: null,
-        tipo: {
-            entrada: "+",
-            saida: "-",
-        },
-        
-        
+    const [list, setList] = useState([]);
+    const [desc, setDesc] = useState("");
+    const [valor, setValor] = useState("");
+    const [tipo, setTipo] = useState("entrada");
+
+    const itemLista = () => {
+        if (!desc || !valor) return; // evita adicionar vazio
+
+        const novo = {
+            data: new Date().toLocaleDateString("pt-BR"),
+            desc: desc,
+            valor: Number(valor),
+            tipo: tipo, // ou saida
+            marcada: false,
+        }
+
+        setList([...list,novo]);
+        setDesc("");
+        setValor("");
     };
     return (
         <>
+            <div id = "adicionar">
+                <h1>Adicionar Entrada</h1>
+                <label>
+                <input
+                    type="radio"
+                    value="entrada"
+                    checked={tipo === "entrada"}
+                    onChange={(e) => setTipo(e.target.value)}
+                />
+                Entrada
+                </label>
+
+                <label>
+                <input
+                    type="radio"
+                    value="saida"
+                    checked={tipo === "saida"}
+                    onChange={(e) => setTipo(e.target.value)}
+                />
+                Saída
+                </label>
+                <input type="text" placeholder="descrição" value = {desc} onChange = {(e) => setDesc(e.target.value)}/>
+                <input type="number" placeholder="valor" value = {valor} onChange={(e) => setValor(e.target.value)} />
+                <button onClick={itemLista}>Enviar</button>
+
+            </div>
+
             <div id = "ListaCorpo">
                 <div id = "ListaCabeçalho">
                     <div id = "fundoMother">
@@ -25,15 +64,15 @@ function Lista (){
                             <p>valor</p>
                         </div>
                     </div>
-
-                    <ol>
-                        
-                    </ol>
-
                 </div>
 
                 <ul>
-
+                      {list.map((item, index) => (
+                        <li key={index}>
+                        <input type="checkbox" checked={item.marcada} readOnly />
+                        {item.data} - {item.desc} - R${item.valor} ({item.tipo})
+                        </li>
+                    ))}
                 </ul>
 
             </div>
